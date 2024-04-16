70ea153e772f77064c348f8057f8a04c31bc7531
Marc Thalen <marcthalen@gmail.com>
2024-04-16 13:15:32 +0200

    update globals

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 export default {
-    NFT_ADDRESS : '0xFa49d7be1a207Ce1c0E4fE810f7a72151fe860Eb',
-    CONTRACT_ADDRESS : '0xFa49d7be1a207Ce1c0E4fE810f7a72151fe860Eb',
+    NFT_ADDRESS : '0x1B66671260509026e0d19237B7F32f54F13C756A',
+    CONTRACT_ADDRESS : '0x1B66671260509026e0d19237B7F32f54F13C756A',
     // BACKEND_URL: 'http://localhost:3000',
     BACKEND_URL: 'https://voyager.api.bitcoin.com',
     BUCKET_URL: 'https://bcom-verse-voyager.s3.us-east-1.amazonaws.com',
